2408d51882f670374aa1a7fa6817c61be005d0ef
mac <benard2454adewole@gmail.com>
2025-12-24 11:13:55 -0500

    implemented decision trees for email problem

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -25,6 +26,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print (accuracy_score(labels_test, pred))
+print (len(features_train[0]))
 
 #########################################################
 
