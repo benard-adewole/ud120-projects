6d3bfea8673f7cd19449c68728651256658797bd
mac <benard2454adewole@gmail.com>
2025-12-24 11:14:32 -0500

    implemented svm for email problem

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -18,11 +18,39 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
+chopDataset = False
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import numpy as np
 
+if chopDataset:
+    features_train = features_train[:int(len(features_train)//100)] 
+    labels_train = labels_train[:int(len(labels_train)//100)]
+
+clf = SVC(C=10000,kernel='rbf')
+
+# train moddel
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+# inference
+t0 = time()
+pred = clf.predict(features_test)
+
+print(np.unique_counts(pred))
+print(f'Counts for [Sara, Chris]: {np.bincount(pred)}')
+
+print(clf.predict(features_test[10:11]))
+print(clf.predict(features_test[26:27]))
+print(clf.predict(features_test[50:51]))
+
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+print(f'Accuracy: {accuracy_score(labels_test, pred)}')
 
 #########################################################
 
